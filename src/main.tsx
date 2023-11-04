23f5ca3552da50c74bd4508bfcc38b90deed0d23
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-04 19:04:23 +0200

    Refactor: Update main.tsx for MUI and Router integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+declare module "@mui/material/styles" {
+  interface Theme {
+    creamy: string;
+    deepBlue: string;
+    orange: string;
+    midnightNavy: string;
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    creamy?: string;
+    deepBlue?: string;
+    orange?: string;
+    midnightNavy?: string;
+  }
+}
+const theme = createTheme({
+  creamy: "#FBF3F2",
+  deepBlue: "#1C768F",
+  orange: "#FA991C",
+  midnightNavy: "#032539",
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
