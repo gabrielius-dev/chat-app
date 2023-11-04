d39752589a86ce709a3126b2151194250b9f8535
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-04 20:40:21 +0200

    Add transformError function

---
+++ src/components/helpers.ts
@@ -0,0 +1,19 @@
+import { ErrorInterface } from "./types/Error";
+
+interface ErrorObject {
+  password?: string[];
+  username?: string[];
+}
+
+export function transformError(errorArray: ErrorInterface[]): ErrorObject {
+  type FormattedData = Record<string, string[]>;
+  const formattedData: FormattedData = {};
+  errorArray.forEach((item) => {
+    if (formattedData[item.path]) {
+      formattedData[item.path].push(item.message);
+    } else {
+      formattedData[item.path] = [item.message];
+    }
+  });
+  return formattedData;
+}
