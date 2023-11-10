6f2f0f632c258d5374183efe3ca72b0e90cfb441
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-10 21:37:43 +0200

    Add tests for Login component

---
+++ src/components/GuestComponents/__tests__/Login.test.tsx
@@ -0,0 +1,121 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Login from "../Login";
+import { MockedFunction, vi } from "vitest";
+import axios from "axios";
+
+vi.mock("axios");
+
+test("Submit function gets called", async () => {
+  const setUserMock = vi.fn();
+  render(<Login setUser={setUserMock} />);
+
+  const userMock = { _id: "ID", username: "Username", password: "Password" };
+
+  const usernameInput = screen.getByLabelText("Username");
+  await userEvent.type(usernameInput, "test");
+  expect(usernameInput).toHaveValue("test");
+
+  const passwordInput = screen.getByLabelText("Password");
+  await userEvent.type(passwordInput, "test");
+  expect(passwordInput).toHaveValue("test");
+
+  (axios.post as MockedFunction<typeof axios.post>).mockResolvedValue({
+    data: { user: userMock },
+  });
+
+  await userEvent.click(screen.getByRole("button", { name: "Login" }));
+
+  expect(setUserMock).toHaveBeenCalledWith(userMock);
+});
+
+test("Empty form submission", async () => {
+  const setUserMock = vi.fn();
+  render(<Login setUser={setUserMock} />);
+  await userEvent.click(screen.getByRole("button", { name: "Login" }));
+  const usernameError = screen.getByTestId("username-error-element");
+  expect(usernameError).toHaveTextContent("Username must be specified");
+  const passwordError = screen.getByTestId("password-error-element");
+  expect(passwordError).toHaveTextContent("Password must be specified");
+});
+
+test("Display server errors", async () => {
+  const setUserMock = vi.fn();
+  render(<Login setUser={setUserMock} />);
+
+  const errorMock = [
+    { path: "username", message: "SERVER_ERROR" },
+    { path: "password", message: "SERVER_ERROR" },
+  ];
+
+  const usernameInput = screen.getByLabelText("Username");
+  await userEvent.type(usernameInput, "test");
+  expect(usernameInput).toHaveValue("test");
+
+  const passwordInput = screen.getByLabelText("Password");
+  await userEvent.type(passwordInput, "test");
+  expect(passwordInput).toHaveValue("test");
+
+  (axios.post as MockedFunction<typeof axios.post>).mockRejectedValue({
+    response: {
+      data: { errors: errorMock },
+    },
+  });
+
+  await userEvent.click(screen.getByRole("button", { name: "Login" }));
+  const usernameError = screen.getByTestId("username-error-element");
+  expect(usernameError).toHaveTextContent("SERVER_ERROR");
+  const passwordError = screen.getByTestId("password-error-element");
+  expect(passwordError).toHaveTextContent("SERVER_ERROR");
+});
+
+test("Username and password exceed 100 characters", async () => {
+  const setUserMock = vi.fn();
+  render(<Login setUser={setUserMock} />);
+
+  const usernameInput = screen.getByLabelText("Username");
+  await userEvent.type(
+    usernameInput,
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  );
+  expect(usernameInput).toHaveValue(
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  );
+
+  const passwordInput = screen.getByLabelText("Password");
+  await userEvent.type(
+    passwordInput,
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  );
+  expect(passwordInput).toHaveValue(
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  );
+
+  await userEvent.click(screen.getByRole("button", { name: "Login" }));
+  const usernameError = screen.getByTestId("username-error-element");
+  expect(usernameError).toHaveTextContent(
+    "Username can't exceed 100 characters"
+  );
+  const passwordError = screen.getByTestId("password-error-element");
+  expect(passwordError).toHaveTextContent(
+    "Password can't exceed 100 characters"
+  );
+});
+
+test("Password visibility toggle", async () => {
+  const setUserMock = vi.fn();
+
+  render(<Login setUser={setUserMock} />);
+
+  const togglePasswordButton = screen.getByLabelText(
+    "toggle password visibility"
+  );
+  await userEvent.click(togglePasswordButton);
+
+  const passwordInput = screen.getByLabelText("Password");
+  expect(passwordInput).toHaveAttribute("type", "text");
+
+  await userEvent.click(togglePasswordButton);
+  expect(passwordInput).toHaveAttribute("type", "password");
+});
