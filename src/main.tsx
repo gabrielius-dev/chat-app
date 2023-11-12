ae71a0c1ef216a0849271cec32be1cc62346da36
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-12 22:01:01 +0200

    Add ReactQueryDevtools for development

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 declare module "@mui/material/styles" {
   interface Theme {
@@ -37,6 +38,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <App />
+          <ReactQueryDevtools />
         </QueryClientProvider>
       </ThemeProvider>
     </BrowserRouter>
