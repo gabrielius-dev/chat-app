59463417dbe9a0d5e4478d8608dfb615b1b03bdd
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-15 22:25:27 +0200

    Add Header component

---
+++ src/components/UserComponents/Header.tsx
@@ -0,0 +1,50 @@
+import {
+  Avatar,
+  Box,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { UserInterface } from "../types/User";
+import { Link } from "react-router-dom";
+
+function Header({ user }: { user: UserInterface }) {
+  const theme = useTheme();
+  const isMediumScreen = useMediaQuery(theme.breakpoints.up("md"));
+
+  return (
+    <Box
+      sx={{
+        boxShadow: 1,
+        p: 1,
+        m: 1,
+        borderRadius: 50,
+        display: `${isMediumScreen ? "none" : "flex"}`,
+        alignItems: "center",
+        gap: 1,
+      }}
+    >
+      <Link to="/">
+        <img src="src\assets\illustrations\chat-logo.svg" width={"50px"} />
+      </Link>
+      <Link to="/">
+        <Typography variant="h5" sx={{ color: theme.midnightNavy }}>
+          Chat app
+        </Typography>
+      </Link>
+      <div style={{ marginLeft: "auto" }}>
+        <Link to={user._id}>
+          <Avatar
+            alt="Profile picture"
+            src={user?.img}
+            sx={{ width: 40, height: 40 }}
+          >
+            {!user?.img ? user?.username[0] : null}
+          </Avatar>
+        </Link>
+      </div>
+    </Box>
+  );
+}
+
+export default Header;
