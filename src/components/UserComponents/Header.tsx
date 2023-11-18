426f15437fc413fcdbe621cd6587447edfb2407c
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-18 18:01:06 +0200

    Change responsive design styling

--- src/components/UserComponents/Header.tsx
+++ src/components/UserComponents/Header.tsx
@@ -6,10 +6,9 @@ import {
   MenuItem,
   Snackbar,
   Typography,
-  useMediaQuery,
   useTheme,
 } from "@mui/material";
-import { UserInterface } from "../types/User";
+import { User } from "../types/User";
 import { Link } from "react-router-dom";
 import { Fragment, memo, useState } from "react";
 import PopupState, { bindMenu, bindTrigger } from "material-ui-popup-state";
@@ -17,9 +16,8 @@ import { PopupState as PopupStateType } from "material-ui-popup-state/hooks";
 import axios from "axios";
 import { useQueryClient } from "@tanstack/react-query";
 
-const Header = memo(function Header({ user }: { user: UserInterface }) {
+const Header = memo(function Header({ user }: { user: User }) {
   const theme = useTheme();
-  const isMediumScreen = useMediaQuery(theme.breakpoints.up("md"));
   const queryClient = useQueryClient();
   const [open, setOpen] = useState(false);
 
@@ -45,73 +43,65 @@ const Header = memo(function Header({ user }: { user: UserInterface }) {
   };
 
   return (
-    <>
-      {!isMediumScreen && (
-        <Box
-          sx={{
-            boxShadow: 1,
-            p: 1,
-            m: 1,
-            borderRadius: 50,
-            display: "flex",
-            alignItems: "center",
-            gap: 1,
-          }}
+    <Box
+      sx={{
+        boxShadow: 1,
+        p: 1,
+        m: 1,
+        borderRadius: 50,
+        display: "flex",
+        alignItems: "center",
+        gap: 1,
+      }}
+    >
+      <Link to="/">
+        <img src="src\assets\illustrations\chat-logo.svg" width={"50px"} />
+      </Link>
+      <Link to="/">
+        <Typography variant="h5" sx={{ color: theme.midnightNavy }}>
+          Chat app
+        </Typography>
+      </Link>
+      <div style={{ marginLeft: "auto" }}>
+        <PopupState
+          variant="popover"
+          popupId="popup-menu"
+          disableAutoFocus={true}
         >
-          <Link to="/">
-            <img src="src\assets\illustrations\chat-logo.svg" width={"50px"} />
-          </Link>
-          <Link to="/">
-            <Typography variant="h5" sx={{ color: theme.midnightNavy }}>
-              Chat app
-            </Typography>
-          </Link>
-          <div style={{ marginLeft: "auto" }}>
-            <PopupState
-              variant="popover"
-              popupId="popup-menu"
-              disableAutoFocus={true}
-            >
-              {(popupState) => (
-                <Fragment>
-                  <Avatar
-                    alt="Profile picture"
-                    src={user?.img}
-                    sx={{
-                      width: 40,
-                      height: 40,
-                      bgcolor: theme.deepBlue,
-                      cursor: "pointer",
-                    }}
-                    {...bindTrigger(popupState)}
-                    data-testid="popup-open"
-                  >
-                    {!user?.img ? user?.username[0].toUpperCase() : null}
-                  </Avatar>
-                  <Menu {...bindMenu(popupState)}>
-                    <MenuItem onClick={popupState.close}>
-                      <Link to="/profile">My account</Link>
-                    </MenuItem>
-                    <MenuItem onClick={() => void handleLogout(popupState)}>
-                      Logout
-                    </MenuItem>
-                  </Menu>
-                </Fragment>
-              )}
-            </PopupState>
-          </div>
-          <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
-            <Alert
-              onClose={handleClose}
-              severity="error"
-              sx={{ width: "100%" }}
-            >
-              An error occurred during logout. Please try again later.
-            </Alert>
-          </Snackbar>
-        </Box>
-      )}
-    </>
+          {(popupState) => (
+            <Fragment>
+              <Avatar
+                alt="Profile picture"
+                src={user?.img}
+                sx={{
+                  width: 40,
+                  height: 40,
+                  bgcolor: theme.deepBlue,
+                  cursor: "pointer",
+                }}
+                {...bindTrigger(popupState)}
+                data-testid="popup-open"
+              >
+                {!user?.img ? user?.username[0].toUpperCase() : null}
+              </Avatar>
+              <Menu {...bindMenu(popupState)}>
+                <MenuItem onClick={popupState.close}>
+                  <Link to="/profile">My account</Link>
+                </MenuItem>
+                <MenuItem onClick={() => void handleLogout(popupState)}>
+                  Logout
+                </MenuItem>
+              </Menu>
+            </Fragment>
+          )}
+        </PopupState>
+      </div>
+      <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+          An error occurred during logout. Please try again later.
+        </Alert>
+      </Snackbar>
+    </Box>
   );
 });
 
