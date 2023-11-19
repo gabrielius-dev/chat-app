874ca931b1e4f92b385cd6eefce3264eae468e39
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-19 19:45:01 +0200

    Remove redundant code

--- src/components/GuestComponents/Main.tsx
+++ src/components/GuestComponents/Main.tsx
@@ -10,68 +10,60 @@ import { useState } from "react";
 import WelcomeBox from "./WelcomeBox";
 import Login from "./Login";
 import SignUp from "./SignUp";
-import { useQueryClient } from "@tanstack/react-query";
-import { User } from "../types/User";
 
 function Main() {
   const theme = useTheme();
   const [showLogin, setShowLogin] = useState(true);
   const isMediumScreen = useMediaQuery(theme.breakpoints.up("md"));
-  const queryClient = useQueryClient();
-  const user: User | undefined = queryClient.getQueryData(["userData"]);
 
   return (
-    !user && (
-      <Container
-        maxWidth="xl"
-        sx={{
-          height: "100vh",
-          display: `${isMediumScreen ? "flex" : "block"}`,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        component="main"
-      >
-        <Grid container sx={{ marginY: 2 }}>
-          <WelcomeBox />
+    <Container
+      maxWidth="xl"
+      sx={{
+        height: "100vh",
+        display: `${isMediumScreen ? "flex" : "block"}`,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+      component="main"
+    >
+      <Grid container sx={{ marginY: 2 }}>
+        <WelcomeBox />
 
-          <Grid
-            item
-            xs={true}
-            md={6}
-            sx={{
-              boxShadow: 1,
-              p: 2,
-              borderRadius: 1,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              mb: `${isMediumScreen ? 0 : 4}`,
-            }}
-          >
-            {showLogin && <Login />}
-            {!showLogin && <SignUp />}
-            <Typography sx={{ textAlign: "center" }}>
-              {showLogin
-                ? "Don't have an account?"
-                : "Already have an account?"}{" "}
-              <Link
-                sx={{ color: theme.deepBlue, fontWeight: "bold" }}
-                href="#"
-                underline="none"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setShowLogin(!showLogin);
-                }}
-              >
-                {showLogin ? "Sign up" : "Login"}
-              </Link>
-            </Typography>
-          </Grid>
+        <Grid
+          item
+          xs={true}
+          md={6}
+          sx={{
+            boxShadow: 1,
+            p: 2,
+            borderRadius: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            mb: `${isMediumScreen ? 0 : 4}`,
+          }}
+        >
+          {showLogin && <Login />}
+          {!showLogin && <SignUp />}
+          <Typography sx={{ textAlign: "center" }}>
+            {showLogin ? "Don't have an account?" : "Already have an account?"}{" "}
+            <Link
+              sx={{ color: theme.deepBlue, fontWeight: "bold" }}
+              href="#"
+              underline="none"
+              onClick={(e) => {
+                e.preventDefault();
+                setShowLogin(!showLogin);
+              }}
+            >
+              {showLogin ? "Sign up" : "Login"}
+            </Link>
+          </Typography>
         </Grid>
-      </Container>
-    )
+      </Grid>
+    </Container>
   );
 }
 
