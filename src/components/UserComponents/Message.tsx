c37577511baf5713c651df3d9e601bee624927f6
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-19 21:53:48 +0200

    Add Message component

---
+++ src/components/UserComponents/Message.tsx
@@ -0,0 +1,53 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { MessageInterface } from "../types/Message";
+import { User } from "../types/User";
+import { Box, useTheme, Typography } from "@mui/material";
+import formatCustomDate from "../utils/formatDate";
+
+function Message({ message }: { message: MessageInterface }) {
+  const queryClient = useQueryClient();
+  const theme = useTheme();
+  const user: User = queryClient.getQueryData(["userData"])!;
+  const selectedUser: User = queryClient.getQueryData(["databaseUserData"])!;
+
+  return (
+    <Box
+      sx={{
+        maxWidth: "70%",
+        ml: `${message.sender === user._id ? "auto" : "0"}`,
+        mr: `${message.sender === selectedUser._id ? "auto" : "0"}`,
+        mb: 2,
+      }}
+    >
+      <Box
+        sx={{
+          borderRadius: 7,
+          backgroundColor: `${
+            message.sender === user._id ? theme.deepBlue : "white"
+          }`,
+          color: `${message.sender === user._id ? "white" : "black"}`,
+          py: 2,
+          px: 3,
+          boxShadow: 1,
+        }}
+      >
+        <Typography
+          variant="body1"
+          sx={{
+            wordBreak: "break-word",
+            overflowWrap: "break-word",
+          }}
+        >
+          {message.content}
+        </Typography>
+      </Box>
+      <Typography
+        sx={{ color: "rgba(0, 0, 0, 0.6)", textAlign: "right", pr: 4 }}
+      >
+        {formatCustomDate(message.createdAt)}
+      </Typography>
+    </Box>
+  );
+}
+
+export default Message;
