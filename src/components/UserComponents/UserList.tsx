ddb7e9e514f2f501232ef7f4e68515876fe859f9
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-19 18:53:41 +0200

    Add loading screen

--- src/components/UserComponents/UserList.tsx
+++ src/components/UserComponents/UserList.tsx
@@ -11,16 +11,20 @@ import {
   useTheme,
   Button,
   Box,
+  CircularProgress,
 } from "@mui/material";
 import TimeAgo from "react-timeago";
 import { Link } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
 
-const UserList = memo(function UserList({ user }: { user: User }) {
+const UserList = memo(function UserList() {
   const theme = useTheme();
   const [userList, setUserList] = useState<User[] | []>([]);
   const [loadOffset, setLoadOffset] = useState(1);
   const [moreUsersExist, setMoreUsersExist] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const queryClient = useQueryClient();
+  const user: User = queryClient.getQueryData(["userData"])!;
 
   useEffect(() => {
     setIsLoading(true);
@@ -44,86 +48,86 @@ const UserList = memo(function UserList({ user }: { user: User }) {
         setIsLoading(false);
       })
       .catch((err) => console.error(err));
-  }, [loadOffset, user._id]);
+  }, [loadOffset]);
 
   const loadMoreUsers = () =>
     setLoadOffset((currentLoadOffset) => currentLoadOffset + 1);
 
   return (
-    !isLoading && (
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          mb: 2,
-        }}
-      >
-        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-          {userList.map((listUser) => (
-            <Link key={listUser._id} to={`/messages/${listUser._id}`}>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemAvatar>
-                    <Avatar
-                      alt="Profile picture"
-                      src={listUser?.img}
-                      sx={{ width: 40, height: 40, bgcolor: theme.deepBlue }}
-                    >
-                      {!listUser?.img
-                        ? listUser?.username[0].toUpperCase()
-                        : null}
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary={listUser.username}
-                    secondary={
-                      listUser.latestMessage
-                        ? `${
-                            listUser.latestMessage?.sender === user._id
-                              ? "You: "
-                              : ""
-                          }${
-                            listUser.latestMessage?.message?.length > 20
-                              ? listUser.latestMessage.message.slice(0, 20) +
-                                "..."
-                              : listUser.latestMessage.message
-                          }`
-                        : `Say hi to ${listUser.username}!`
-                    }
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        mb: 2,
+        alignItems: "center",
+      }}
+    >
+      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+        {userList.map((listUser) => (
+          <Link key={listUser._id} to={`/messages/${listUser._id}`}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar
+                    alt="Profile picture"
+                    src={listUser?.img}
+                    sx={{ width: 40, height: 40, bgcolor: theme.deepBlue }}
+                  >
+                    {!listUser?.img
+                      ? listUser?.username[0].toUpperCase()
+                      : null}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary={listUser.username}
+                  secondary={
+                    listUser.latestMessage
+                      ? `${
+                          listUser.latestMessage?.sender === user._id
+                            ? "You: "
+                            : ""
+                        }${
+                          listUser.latestMessage?.message?.length > 20
+                            ? listUser.latestMessage.message.slice(0, 20) +
+                              "..."
+                            : listUser.latestMessage.message
+                        }`
+                      : `Say hi to ${listUser.username}!`
+                  }
+                />
+                {listUser.latestMessage?.createdAt && (
+                  <TimeAgo
+                    date={listUser.latestMessage?.createdAt}
+                    minPeriod={60}
+                    style={{ color: "rgba(0, 0, 0, 0.6)" }}
                   />
-                  {listUser.latestMessage?.createdAt && (
-                    <TimeAgo
-                      date={listUser.latestMessage?.createdAt}
-                      minPeriod={60}
-                      style={{ color: "rgba(0, 0, 0, 0.6)" }}
-                    />
-                  )}
-                </ListItemButton>
-              </ListItem>
-            </Link>
-          ))}
-        </List>
-        {moreUsersExist && (
-          <Button
-            variant="outlined"
-            sx={{
-              color: theme.deepBlue,
-              fontWeight: "bold",
-              textTransform: "none",
-              "&:hover": {
-                border: 2,
-              },
-              borderRadius: 1,
+                )}
+              </ListItemButton>
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      {isLoading && <CircularProgress sx={{ color: theme.deepBlue }} />}
+      {!isLoading && moreUsersExist && (
+        <Button
+          variant="outlined"
+          sx={{
+            color: theme.deepBlue,
+            fontWeight: "bold",
+            textTransform: "none",
+            "&:hover": {
               border: 2,
-              marginX: 1,
-            }}
-            onClick={loadMoreUsers}
-          >
-            Load more
-          </Button>
-        )}
-      </Box>
-    )
+            },
+            borderRadius: 1,
+            border: 2,
+            marginX: 1,
+          }}
+          onClick={loadMoreUsers}
+        >
+          Load more
+        </Button>
+      )}
+    </Box>
   );
 });
 
