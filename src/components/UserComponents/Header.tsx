d0d6cb9c4c9f9270fb7e6dafab81cc876cbf3914
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-11-25 22:38:45 +0200

    Directly use svg for better loading speed

--- src/components/UserComponents/Header.tsx
+++ src/components/UserComponents/Header.tsx
@@ -15,7 +15,6 @@ import PopupState, { bindMenu, bindTrigger } from "material-ui-popup-state";
 import { PopupState as PopupStateType } from "material-ui-popup-state/hooks";
 import axios from "axios";
 import { useQueryClient } from "@tanstack/react-query";
-import ChatLogo from "../../assets/illustrations/chat-logo.svg";
 
 function Header() {
   const theme = useTheme();
@@ -23,11 +22,6 @@ function Header() {
   const [open, setOpen] = useState(false);
   const user: User = queryClient.getQueryData(["userData"])!;
   const navigate = useNavigate();
-  const [isImageLoaded, setIsImageLoaded] = useState(false);
-
-  const handleImageLoad = () => {
-    setIsImageLoaded(true);
-  };
 
   async function handleLogout(popupState: PopupStateType) {
     try {
@@ -61,11 +55,46 @@ function Header() {
         display: "flex",
         alignItems: "center",
         gap: 1,
-        visibility: isImageLoaded ? "visible" : "hidden",
       }}
     >
       <Link to="/">
-        <img src={ChatLogo} width={"50px"} onLoad={handleImageLoad} />
+        <svg
+          width="50px"
+          height="50px"
+          viewBox="-2.4 -2.4 28.80 28.80"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="#1C768F"
+          transform="rotate(0)"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+
+          <g id="SVGRepo_iconCarrier">
+            <path
+              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22Z"
+              stroke="#1C274C"
+              strokeWidth="1.5"
+            />
+            <path
+              d="M8 10.5H16"
+              stroke="#1C274C"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
+            <path
+              d="M8 14H13.5"
+              stroke="#1C274C"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            />
+          </g>
+        </svg>
       </Link>
       <Link to="/">
         <Typography variant="h5" sx={{ color: theme.midnightNavy }}>
