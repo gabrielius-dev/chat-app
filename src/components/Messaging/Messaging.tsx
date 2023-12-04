d7bcbeab06d515fd02c13435c8d8d157e6c34e24
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-04 22:25:10 +0200

    Export Sidebar and Error components to reduce re-rendering

--- src/components/Messaging/Messaging.tsx
+++ src/components/Messaging/Messaging.tsx
@@ -9,7 +9,7 @@ import {
   InputBase,
   useMediaQuery,
 } from "@mui/material";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useQueryClient } from "@tanstack/react-query";
 import {
   useCallback,
@@ -24,11 +24,19 @@ import TimeAgo from "react-timeago";
 import WindowFocusContext from "../../context/WindowsFocusContext";
 import Messages from "./Messages";
 import socket from "../../socket/socket";
-import Error from "../Error/Error";
 import LoadingScreen from "../UtilityComponents/LoadingScreen";
-import Sidebar from "../Sidebar/Sidebar";
 
-function Messaging({ isSocketConnected }: { isSocketConnected: boolean }) {
+type setOpenType = (open: boolean) => void;
+
+function Messaging({
+  isSocketConnected,
+  open,
+  setOpen,
+}: {
+  isSocketConnected: boolean;
+  open: boolean;
+  setOpen: setOpenType;
+}) {
   const theme = useTheme();
   const queryClient = useQueryClient();
   const user: User = queryClient.getQueryData(["userData"])!;
@@ -48,11 +56,7 @@ function Messaging({ isSocketConnected }: { isSocketConnected: boolean }) {
   const [selectedUserExists, setSelectedUserExists] = useState(false);
   const [showLoadingScreen, setShowLoadingScreen] = useState(false);
   const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
-  const [open, setOpen] = useState(isSmallScreen);
-
-  const toggleSidebar = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
+  const navigate = useNavigate();
 
   // Cleanup function when users from user list are selected (Messaging component doesn't unmount, just the selectedUserId changes)
   useEffect(() => {
@@ -65,7 +69,7 @@ function Messaging({ isSocketConnected }: { isSocketConnected: boolean }) {
 
   useEffect(() => {
     setOpen(isSmallScreen);
-  }, [isSmallScreen]);
+  }, [isSmallScreen, setOpen]);
 
   useEffect(() => {
     setTimeout(() => {
@@ -257,156 +261,146 @@ function Messaging({ isSocketConnected }: { isSocketConnected: boolean }) {
     }
   };
 
+  useEffect(() => {
+    if (!selectedUser && !isLoadingSelectedUser) {
+      navigate("/user-not-found");
+    }
+  }, [isLoadingSelectedUser, navigate, selectedUser]);
+
   return (
     <>
       {!isInitialMessageFetching && isSocketConnected && selectedUser && (
         <Box
           sx={{
-            display: "flex",
-            overflow: "hidden",
+            display: open && !isSmallScreen ? "none" : "flex",
+            flexDirection: "column",
+            flex: 1,
             height: "100%",
-            position: "relative",
+            overflow: "auto",
           }}
         >
-          <Sidebar
-            toggleSidebar={toggleSidebar}
-            open={open}
-            isSmallScreen={isSmallScreen}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+
+              p: 2,
+              gap: 2,
+              borderBottom: `1px solid ${theme.lightGray}`,
+            }}
+          >
+            <Link to={`/profile/${selectedUser._id}`}>
+              <Avatar
+                alt="Profile picture"
+                src={selectedUser?.img}
+                sx={{
+                  width: 50,
+                  height: 50,
+                  bgcolor: theme.deepBlue,
+                }}
+              >
+                {!selectedUser?.img
+                  ? selectedUser?.username[0].toUpperCase()
+                  : null}
+              </Avatar>
+            </Link>
+            <Box>
+              <Link to={`/profile/${selectedUser._id}`}>
+                <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                  {selectedUser.username}
+                </Typography>
+              </Link>
+              <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+                <Typography
+                  variant="subtitle1"
+                  sx={{ color: "rgba(0, 0, 0, 0.6)" }}
+                >
+                  {selectedUser.online ? "Online" : "Offline"}
+                </Typography>
+                <TimeAgo
+                  date={selectedUser.lastOnline}
+                  minPeriod={60}
+                  style={{ color: "rgba(0, 0, 0, 0.6)", fontSize: "1rem" }}
+                />
+              </Box>
+            </Box>
+          </Box>
+          <Messages
+            messages={messages}
+            messagesEndRef={messagesEndRef}
+            handleScroll={handleScroll}
+            messagesContainerRef={messagesContainerRef}
           />
           <Box
             sx={{
-              display: open && !isSmallScreen ? "none" : "flex",
-              flexDirection: "column",
-              flex: 1,
-              height: "100%",
+              p: 2,
+              display: "flex",
+              alignItems: "center",
+              gap: 2,
+              borderTop: `1px solid ${theme.lightGray}`,
             }}
           >
-            <Box
+            <InputBase
+              placeholder="Type a message here..."
+              value={message}
+              onInput={handleInputChange}
+              multiline
+              autoFocus
+              required
+              error={!isMessageValid}
+              inputProps={{ maxLength: 1000, spellCheck: false }}
               sx={{
-                display: "flex",
-                alignItems: "center",
-
-                p: 2,
-                gap: 2,
-                borderBottom: `1px solid ${theme.lightGray}`,
+                flex: 1,
+                padding: 2,
+                borderRadius: 10,
+                boxShadow: 1,
+                border: "1px solid transparent",
+                "&.Mui-error": {
+                  border: isMessageValid ? undefined : "1px solid red",
+                },
               }}
-            >
-              <Link to={`/profile/${selectedUser._id}`}>
-                <Avatar
-                  alt="Profile picture"
-                  src={selectedUser?.img}
-                  sx={{
-                    width: 50,
-                    height: 50,
-                    bgcolor: theme.deepBlue,
-                  }}
-                >
-                  {!selectedUser?.img
-                    ? selectedUser?.username[0].toUpperCase()
-                    : null}
-                </Avatar>
-              </Link>
-              <Box>
-                <Link to={`/profile/${selectedUser._id}`}>
-                  <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                    {selectedUser.username}
-                  </Typography>
-                </Link>
-                <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
-                  <Typography
-                    variant="subtitle1"
-                    sx={{ color: "rgba(0, 0, 0, 0.6)" }}
-                  >
-                    {selectedUser.online ? "Online" : "Offline"}
-                  </Typography>
-                  <TimeAgo
-                    date={selectedUser.lastOnline}
-                    minPeriod={60}
-                    style={{ color: "rgba(0, 0, 0, 0.6)", fontSize: "1rem" }}
-                  />
-                </Box>
-              </Box>
-            </Box>
-            <Messages
-              messages={messages}
-              messagesEndRef={messagesEndRef}
-              handleScroll={handleScroll}
-              messagesContainerRef={messagesContainerRef}
             />
-            <Box
-              sx={{
-                p: 2,
+            <button
+              onClick={handleMessageSubmit}
+              style={{
+                width: 50,
+                height: 50,
+                backgroundColor: theme.deepBlue,
+                borderRadius: 50,
                 display: "flex",
+                justifyContent: "center",
                 alignItems: "center",
-                gap: 2,
-                borderTop: `1px solid ${theme.lightGray}`,
               }}
             >
-              <InputBase
-                placeholder="Type a message here..."
-                value={message}
-                onInput={handleInputChange}
-                multiline
-                autoFocus
-                required
-                error={!isMessageValid}
-                inputProps={{ maxLength: 1000, spellCheck: false }}
-                sx={{
-                  flex: 1,
-                  padding: 2,
-                  borderRadius: 10,
-                  boxShadow: 1,
-                  border: "1px solid transparent",
-                  "&.Mui-error": {
-                    border: isMessageValid ? undefined : "1px solid red",
-                  },
-                }}
-              />
-              <button
-                onClick={handleMessageSubmit}
-                style={{
-                  width: 50,
-                  height: 50,
-                  backgroundColor: theme.deepBlue,
-                  borderRadius: 50,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
+              <svg
+                fill="#fff"
+                height="30px"
+                width="30px"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 495.003 495.003"
+                xmlSpace="preserve"
               >
-                <svg
-                  fill="#fff"
-                  height="30px"
-                  width="30px"
-                  version="1.1"
-                  id="Capa_1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  viewBox="0 0 495.003 495.003"
-                  xmlSpace="preserve"
-                >
-                  <g id="XMLID_51_">
-                    <path
-                      id="XMLID_53_"
-                      d="M164.711,456.687c0,2.966,1.647,5.686,4.266,7.072c2.617,1.385,5.799,1.207,8.245-0.468l55.09-37.616
+                <g id="XMLID_51_">
+                  <path
+                    id="XMLID_53_"
+                    d="M164.711,456.687c0,2.966,1.647,5.686,4.266,7.072c2.617,1.385,5.799,1.207,8.245-0.468l55.09-37.616
 		l-67.6-32.22V456.687z"
-                    />
-                    <path
-                      id="XMLID_52_"
-                      d="M492.431,32.443c-1.513-1.395-3.466-2.125-5.44-2.125c-1.19,0-2.377,0.264-3.5,0.816L7.905,264.422
+                  />
+                  <path
+                    id="XMLID_52_"
+                    d="M492.431,32.443c-1.513-1.395-3.466-2.125-5.44-2.125c-1.19,0-2.377,0.264-3.5,0.816L7.905,264.422
 		c-4.861,2.389-7.937,7.353-7.904,12.783c0.033,5.423,3.161,10.353,8.057,12.689l125.342,59.724l250.62-205.99L164.455,364.414
 		l156.145,74.4c1.918,0.919,4.012,1.376,6.084,1.376c1.768,0,3.519-0.322,5.186-0.977c3.637-1.438,6.527-4.318,7.97-7.956
 		L494.436,41.257C495.66,38.188,494.862,34.679,492.431,32.443z"
-                    />
-                  </g>
-                </svg>
-              </button>
-            </Box>
+                  />
+                </g>
+              </svg>
+            </button>
           </Box>
         </Box>
-      )}
-      {!selectedUser && !isLoadingSelectedUser && (
-        <Error errorMessage="User not found" />
       )}
       {isLoadingSelectedUser &&
         isInitialMessageFetching &&
