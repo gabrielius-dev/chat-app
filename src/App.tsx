0c3c2ff5003386efdd8c73f8bc4646980a658853
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-07 21:33:45 +0200

    Add additional routes and optimize Sidebar loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import axios, { AxiosResponse } from "axios";
 import { User, UserResponse } from "./components/types/User";
 import { useQuery } from "@tanstack/react-query";
 import LoadingScreen from "./components/UtilityComponents/LoadingScreen";
 import Messaging from "./components/Messaging/Messaging";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useCallback } from "react";
 import WindowFocusContext from "./context/WindowsFocusContext";
-import { Box } from "@mui/material";
+import { Box, useTheme, useMediaQuery } from "@mui/material";
 import Error from "./components/Error/Error";
 import socket from "./socket/socket";
 import Index from "./components/Index/Index";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   const isWindowFocused = useContext(WindowFocusContext);
   const [isSocketConnected, setIsSocketConnected] = useState(false);
+  const location = useLocation();
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
+  const [open, setOpen] = useState(isSmallScreen);
+  const [messagingUserExists, setMessagingUserExists] = useState(false);
 
-  const getUser = async () => {
+  const toggleSidebar = useCallback(() => {
+    setOpen((prevOpen) => !prevOpen);
+  }, []);
+
+  const memoizedSetOpen = useCallback(
+    (value: boolean) => {
+      setOpen(value);
+    },
+    [setOpen]
+  );
+
+  const getUser = useCallback(async () => {
     const response: AxiosResponse<UserResponse> = await axios.get(
       "http://localhost:8000/user",
       {
@@ -24,7 +41,7 @@ function App() {
       }
     );
     return response.data.user;
-  };
+  }, []);
 
   const { data: user, isLoading } = useQuery<User, Error>({
     queryKey: ["userData"],
@@ -60,19 +77,50 @@ function App() {
       {!isLoading && (
         <>
           {user && <Header />}
-          <Routes>
-            <Route
-              path="/"
-              element={<Index isSocketConnected={isSocketConnected} />}
-            />
-            {user && (
+          <Box
+            sx={{
+              display: "flex",
+              height: "100vh",
+              overflow: "hidden",
+            }}
+          >
+            {user &&
+              location.pathname.startsWith("/messages/") &&
+              messagingUserExists && (
+                <Sidebar
+                  toggleSidebar={toggleSidebar}
+                  open={open}
+                  isSmallScreen={isSmallScreen}
+                />
+              )}
+            <Routes>
               <Route
-                path="/messages/:selectedUserId"
-                element={<Messaging isSocketConnected={isSocketConnected} />}
+                path="/"
+                element={<Index isSocketConnected={isSocketConnected} />}
               />
-            )}
-            <Route path="*" element={<Error errorMessage="Page not found" />} />
-          </Routes>
+              {user && (
+                <Route
+                  path="/messages/:selectedUserId"
+                  element={
+                    <Messaging
+                      isSocketConnected={isSocketConnected}
+                      open={open}
+                      setOpen={memoizedSetOpen}
+                      setMessagingUserExists={setMessagingUserExists}
+                    />
+                  }
+                />
+              )}
+              <Route
+                path="/user-not-found"
+                element={<Error errorMessage="User not found" />}
+              />
+              <Route
+                path="*"
+                element={<Error errorMessage="Page not found" />}
+              />
+            </Routes>
+          </Box>
         </>
       )}
       {isLoading && <LoadingScreen />}
