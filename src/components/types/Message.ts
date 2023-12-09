0ec532a220df8e3619b000ead4712ed227be28fb
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-09 22:24:15 +0200

    Add Chat interface

--- src/components/types/Message.ts
+++ src/components/types/Message.ts
@@ -1,7 +1,13 @@
+import { User } from "./User";
+
 export interface MessageInterface {
   _id: string;
   content: string;
   createdAt: string;
   sender: string;
   receiver: string;
+}
+
+export interface Chat extends User {
+  latestMessage: MessageInterface;
 }
