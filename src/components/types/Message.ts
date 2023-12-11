ec7a51d03a28cc3cc8b864eefba14023b31a8e63
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-11 21:44:14 +0200

    Remove redundant types

--- src/components/types/Message.ts
+++ src/components/types/Message.ts
@@ -1,13 +1,7 @@
-import { User } from "./User";
-
 export interface MessageInterface {
   _id: string;
   content: string;
   createdAt: string;
   sender: string;
   receiver: string;
-}
-
-export interface Chat extends User {
-  latestMessage: MessageInterface;
 }
