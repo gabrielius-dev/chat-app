bb7a57189fa82f58d97a2d34a33d74225224e7a6
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-25 20:13:22 +0200

    Add additional interfaces for group chat

--- src/components/types/Chat.ts
+++ src/components/types/Chat.ts
@@ -13,3 +13,17 @@ export interface GroupChat {
   createdAt: string;
   latestMessage: MessageInterface;
 }
+
+export interface GroupChatWithoutLatestMessage {
+  _id: string;
+  name: string;
+  image?: string;
+  users: User[];
+  createdAt: string;
+}
+
+export interface GroupChatResponse {
+  success: string;
+  groupChat: GroupChatWithoutLatestMessage;
+  message: string;
+}
