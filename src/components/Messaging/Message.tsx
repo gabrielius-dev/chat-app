9a32af5fcf478ea72a24fc929c60b1af4cd3c97a
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-29 17:10:49 +0200

    Remove message from messages list

--- src/components/Messaging/Message.tsx
+++ src/components/Messaging/Message.tsx
@@ -10,16 +10,34 @@ import {
   Alert,
 } from "@mui/material";
 import formatCustomDate from "../utils/formatDate";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 import axios, { AxiosResponse } from "axios";
+import { useChatContext } from "../../context/useChatContext";
+import { useParams } from "react-router-dom";
 
-function Message({ message }: { message: MessageInterface }) {
+type setMessagesType = Dispatch<SetStateAction<MessageInterface[]>>;
+
+function Message({
+  message,
+  messages,
+  setMessages,
+}: {
+  message: MessageInterface;
+  messages: MessageInterface[];
+  setMessages: setMessagesType;
+}) {
   const queryClient = useQueryClient();
   const theme = useTheme();
   const user: User = queryClient.getQueryData(["userData"])!;
   const [isHovered, setIsHovered] = useState(false);
   const [open, setOpen] = useState(false);
+  const { setChatList } = useChatContext();
+  const { selectedUserId } = useParams();
+  const selectedUser: User = queryClient.getQueryData([
+    "databaseUserData",
+    selectedUserId,
+  ])!;
 
   async function deleteMessage() {
     try {
@@ -27,9 +45,36 @@ function Message({ message }: { message: MessageInterface }) {
         `http://localhost:8000/message/${message._id}`,
         { withCredentials: true }
       );
-      if (res.status !== 204) {
-        setOpen(true);
+      if (res.status === 204) {
+        if (messages.length - 1 === messages.indexOf(message)) {
+          setChatList((prevChatList) =>
+            prevChatList
+              .map((chat) => {
+                if (chat._id === selectedUserId) {
+                  return {
+                    ...selectedUser,
+                    latestMessage: messages[messages.length - 2],
+                  };
+                }
+                return chat;
+              })
+              .sort((a, b) => {
+                const aCreatedAt =
+                  a.latestMessage?.createdAt ?? "0000-00-00T00:00:00.000Z";
+                const bCreatedAt =
+                  b.latestMessage?.createdAt ?? "0000-00-00T00:00:00.000Z";
+                return bCreatedAt.localeCompare(aCreatedAt);
+              })
+          );
+        }
+        setMessages((previousMessages) =>
+          previousMessages.filter(
+            (prevMessage) => prevMessage._id !== message._id
+          )
+        );
       }
+
+      if (res.status !== 204) setOpen(true);
     } catch (err) {
       setOpen(true);
     }
