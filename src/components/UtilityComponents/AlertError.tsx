13c9b36c11b1b4f7413980f5eeccd677629f1819
gabrielius-dev <gabrieliusbutkus2005@gmail.com>
2023-12-30 19:38:47 +0200

    Add AlertError component

---
+++ src/components/UtilityComponents/AlertError.tsx
@@ -0,0 +1,27 @@
+import { Alert, Snackbar } from "@mui/material";
+import { Dispatch, SetStateAction, memo } from "react";
+
+type setOpenType = Dispatch<SetStateAction<boolean>>;
+
+const AlertError = memo(function AlertError({
+  message,
+  open,
+  setOpen,
+}: {
+  message: string;
+  open: boolean;
+  setOpen: setOpenType;
+}) {
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+});
+
+export default AlertError;
